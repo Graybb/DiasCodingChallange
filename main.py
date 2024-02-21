d64cc1d357aabef31fe88a20bcf1380a237500b7
Mathias home <au568660@uni.au.dk>
2024-02-21 14:38:40 +0100

    made constructors and creation of data set with missing admissions

--- main.py
+++ main.py
@@ -1,18 +1,63 @@
 import numpy as np
 from flask import Flask
 from flask_restful import Resource, Api,reqparse
+import random
+patientDict = {}
+doctorLists = []
+AdmissionDict = {}
+
+
+class Admission:
+    def __init__(self,department,patient,doctors =None):
+        self.department = department
+        self.doctors = doctors
+        self.patient = patient
+
+class Patient:
+    def __init__(self,name,SSN,patientID):
+        self.name = name
+        self.SSN = SSN
+        if patientID == None:
+            self.patientID = len(patientDict)+1
+    
+
+class Doctor:
+    def __init__(self, name, id, department):
+        self.name = name
+        self.ID = id
+        self.department = department
+def createDataset():
+    #creates Doctors
+    doctors = [Doctor("Jan Hajsen","001","A"),Doctor("Mani Freidrich","002","B"),Doctor("Josefine Hajsen","003","A"),Doctor("Kim Possible","004","C")]
+    
+    #creates Patients
+    for i in range(8):
+        patient = Patient("Testperson"+i,random.randint(0000,9999))
+        patientDict[patient.patientID] = patient
+
+    doctorWithPatient = {
+        '001': ["001","002","003"],
+        '002': ["007","008"],
+        '003':["002",],
+        '004': ["001","005","006","004"]}    
+    
+
+
+
+
 
 
 
 app = Flask("journal system")
 api = Api(app)
+createDataSet()
 parser = reqparse.RequestParser()
 parser.add_argument()
+
 class Manage(Resource):
-    def get(self,doctor_id):
+    #default method for api, and will return patient if the given doctor has said patient
+    def get(self,doctor_id,patient_id): #using id instead of SSN since the doctor might not be allowed to know SSN if they arent their patient
         return doctor_id
-    def getPatient(self,doctor,patient):
-        return NotImplemented
     def createPatient(self):
         return NotImplemented
     def createDoctor(self):
@@ -25,23 +70,7 @@ class Manage(Resource):
         return NotImplemented
      
 
-api.add_resource(Manage,'/getPatient/<doctor_id>')        
-
-class Admission:
-    department = NotImplemented
-    doctors = NotImplemented
-    medicalJournal = NotImplemented
-
-class Patient:
-    name = NotImplemented
-    SSN = NotImplemented
-    patientID = NotImplemented
-
-class Doctor:
-    name = NotImplemented
-    ID = NotImplemented
-    department = NotImplemented
-
+api.add_resource(Manage,'/<doctor_id>')        
 
 if __name__ == '__main__' :
     app.run()
